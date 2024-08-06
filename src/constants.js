9fa255394ae9226e16734fb41e19db0d7f685570
Dduquejh <johandanielduquehernandez@gmail.com>
2024-08-05 23:15:29 -0500

    Add personal info

--- src/constants.js
+++ src/constants.js
@@ -1,47 +1,74 @@
 export const aboutME = {
     title: "Johan Daniel Duque Hernández",
-    text: "I'm a Systems and Computing engineer student at the Universidad EIA. I'm passionate about technology and programming. I'm always looking for new challenges and learning new things. Actually, I have some knowledge in Java, Python, JavaScript, HTML, CSS, and SQL, also I know how to use in a basic level Docker and in a medium level Git and Linux Terminal. I'm always looking for new opportunities to grow and improve my skills.",
-    imgLink: "./src/assets/img/personalPhoto.jpg"
+    text: "I'm a Systems and Computing engineer student at the Universidad EIA. I'm always looking for new opportunities to grow and improve my skills.npm ",
+    imgLink: "./assets/img/personalPhoto.jpg"
 }
 
 export const skills = [
     {
         name: "Java",
+        level: "Advanced",
+        details: "I am proficient in Java with extensive experience in developing complex functions and classes. I excel in designing and implementing advanced data structures and algorithms, and have a deep understanding of Java's standard libraries and frameworks. I have successfully applied these skills in various projects and courses.",
+    },
+    {
+        name: "Python",
+        level: "Intermediate",
+        details: "I can develop basic scripts and programs using various data structures. I have used Python in different projects and courses at a fundamental level, employing libraries such as Pandas, NumPy, and Matplotlib. I am currently working on expanding my knowledge and skills in Python.",
+    },
+    {
+        name: "SQL",
+        level: "Advanced",
+        details: "I am skilled in working with relational databases (SQL), with a deep understanding of writing complex queries, optimizing performance, manipulating data, and designing efficient database schemas. I can also implement advanced features such as stored procedures, triggers, and manage database security and transactions effectively.",
+    },
+    {
+        name: "HTML",
+        level: "Intermediate",
+        details: "I have advanced skills in HTML, including expertise in semantic elements, complex forms, and performance optimization. I ensure cross-browser compatibility and am proficient in HTML5 features like <canvas>, <video>. I also integrate HTML seamlessly with CSS and JavaScript. I am currently working on improving my skills in responsive design and accessibility.",
+    },
+    {
+        name: "CSS",
+        level: "Beginner",
+        details: "I have beginner skills in CSS, with a basic understanding of styling elements, applying colors, fonts, and spacing. I can create simple layouts using techniques like floats and basic positioning. I am familiar with media queries for basic responsive design and can use CSS to style forms and buttons. I am currently working on improving my skills in CSS3 features like Flexbox and Grid.",
+    },
+    {
+        name: "JavaScript",
+        level: "Beginner",
+        details: "I have beginner skills in JavaScript, with a basic understanding of variables, data types, and simple functions. I am able to manipulate basic DOM elements, handle simple events, and perform basic operations using arrays and objects. I am familiar with basic control flow structures like loops and conditionals, and I am beginning to explore concepts like functions and events. I am currently working on improving my skills in JavaScript to build interactive web applications.",
+    },
+    {
+        name: "React",
+        level: "Beginner",
+        details: "I am a beginner in React, with a basic understanding of creating simple components and managing state using hooks. I am familiar with fundamental concepts such as JSX, props, and component lifecycle. My experience includes building basic user interfaces and applying styles with CSS. I am currently learning more about React's features and best practices.",
+    },
+    {
+        name: "Docker",
+        level: "Medium",
+        details: "I have intermediate skills in Docker, with experience in creating and managing Docker images and containers. I am proficient in using Docker Compose for defining and running multi-container applications. I can configure networks and volumes, and I understand how to monitor and troubleshoot containers. Additionally, I follow basic security practices and have experience integrating Docker with CI/CD pipelines.",
+    },
+    {
+        name: "Git",
         level: "Medium",
         details: "Some text to describe the skill",
     },
     {
-        name: "Python",
-    },
-    {
-        name: "SQL",
-    },
-    {
-        name: "HTML",
-    },
-    {
-        name: "CSS",
-    },
-    {
-        name: "JavaScript",
-    },
-    {
-        name: "React"
-    },
+        name: "Linux",
+        level: "Medium",
+        details: "Some text to describe the skill",
+    }
 ]
 
 export const projects = [
     {
         title: "Game with PaperJS",
-        icon: "./src/assets/icons/projectspaperjs.png",
+        icon: "./assets/icons/projectspaperjs.png",
         details: "This is a game made with PaperJS. The objective of the game is to avoid the obstacles and get the highest score possible. The game has a start screen, a game screen, and an end screen. The game has a score counter and a restart button. I made this project with a partner",
-        imgLink: "./src/assets/icons/projectspaperjs.png",
+        imgLink: "./assets/icons/projectspaperjs.png",
         repoLink: "https://github.com/Dduquejh/PaperJSComputacion",
         additionalInfo: ["JavaScript", "HTML", "CSS"]
     },
     {
         title: "Ere-Interpreter",
-        icon: "./src/assets/icons/projectspaperjs.png",
+        icon: "./assets/icons/projectspaperjs.png",
         details: "This is a project made with a partner. The Ere interpreter is a interpreter that we created using Java.",
         imgLink: "",
         repoLink: "https://github.com/Dduquejh/Ere-Interpreter",
@@ -52,27 +79,27 @@ export const projects = [
 export const hobbies = [
     {
         title: "Music",
-        icon: "./src/assets/icons/hobbiesmusic.png",
+        icon: "./assets/icons/hobbiesmusic.png",
         details: "",
-        imgLink: "./src/assets/icons/hobbiesmusic.png"
+        imgLink: "./assets/icons/hobbiesmusic.png"
     },
     {
         title: "Hobby 2",
-        icon: "./src/assets/icons/hobbiesmusic.png",
+        icon: "./assets/icons/hobbiesmusic.png",
         details: "Change this",
-        imgLink: "./src/assets/icons/hobbiesmusic.png"
+        imgLink: "./assets/icons/hobbiesmusic.png"
     },
 ]
 
 export const accounts = [
     {
         name: "GitHub",
-        iconLink: "src/assets/icons/contactGitHub.png",
+        iconLink: "./assets/icons/contactGitHub.png",
         link: "https://github.com/Dduquejh"
     },
     {
         name: "LinkedIn",
-        iconLink: "src/assets/icons/contactLinkedIn.png",
+        iconLink: "./assets/icons/contactLinkedIn.png",
         link: "www.linkedin.com/in/dduquejh"
     }
 ]
